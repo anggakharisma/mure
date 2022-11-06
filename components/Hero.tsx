cf10cf441ea788d130681c695ed9fcb54d14ba6c
Angga Kharisma <anggaputrakharisma@gmail.com>
2022-11-06 11:18:46 +0700

    chore: make player container chore: make player info card ?

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,17 +1,25 @@
 import Image from "next/image";
-import HeroStyles  from '../styles/Hero.module.css';
+import HeroStyles from "../styles/Hero.module.css";
 
 const Hero = () => {
-	return (
-		<div className={HeroStyles.hero}>
-			<div className={HeroStyles.hero_image_container}>
-				<Image src="/images/hero__image.jpg" alt="hero image left" layout="fill" objectFit="contain" />
-			</div>
-			<div className="m-auto">
-				<h1 className="text-8xl ml-28 w-2/4 leading-snug text-primary font-bold  tracking-widest">SHARE LISTEN DISCOVER</h1>
-			</div>
-		</div>
-	);
-}
+  return (
+    <div className={HeroStyles.hero}>
+      <div className={HeroStyles.hero_image_container}>
+        <Image
+          src="/images/hero__image.jpg"
+          alt="hero image left"
+          layout="fill"
+          objectFit="contain"
+        />
+      </div>
+      <div className="m-auto">
+        <h1 className="text-8xl ml-28 w-2/4 leading-snug text-primary font-bold  tracking-widest">
+          SHARE LISTEN DISCOVER
+        </h1>
+      </div>
+    </div>
+  );
+};
 
 export default Hero;
+
--- components/Player/Info.tsx
+++ components/Player/Info.tsx
@@ -1,5 +1,22 @@
+import InfoStyle from "../../styles/Info.module.css";
+import Image from "next/image";
+
 export const Info = () => {
-	return (
-		<div>Info</div>
-	)
-}
+  return (
+    <div className="flex">
+      <div className="relative mr-8 rounded-full">
+        <Image
+          className="rounded-full"
+          src="/images/hero__image.jpg"
+          alt="this is some alt"
+          width={70}
+          height={70}
+        />
+      </div>
+      <div className="my-auto">
+        <h2 className={InfoStyle.SongTitle}>WONDER POP</h2>
+        <h3 className="font-light text-gray-500">Moe Shop</h3>
+      </div>
+    </div>
+  );
+};

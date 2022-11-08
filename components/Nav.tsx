286c7e951c93a902d2bdab5aa3ac5bced358efe7
anggakharisma <anggaputrakharisma@gmail.com>
2022-11-08 12:09:27 +0700

    chore: change route ??

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,27 +1,27 @@
-import Link from 'next/link';
-import NavStyles from '../styles/Nav.module.css';
+import Link from "next/link";
+
+import NavStyles from "../styles/Nav.module.css";
 const Nav = () => {
-	return (
-		<div className="flex w-5/6 m-auto my-6 p-6">
-			<h1 className="text-5xl mr-24 text-primary font-black tracking-tighter my-auto">MURE</h1>
-			<ul className={NavStyles.NavLinkContainer}>
-				<div className="absolute top-2 left-2 bg-accent w-full h-full -z-10 rounded-full"></div>
-				<div className="absolute top-4 left-4 bg-secondary w-full h-full -z-20 rounded-full"></div>
-				<li className={NavStyles.NavLink}>
-					<Link href="/">HOME</Link>
-				</li>
-				<li className={NavStyles.NavLink}>
-					<Link href="/top-artists">TOP ARTISTS</Link>
-				</li>
-				<li className={NavStyles.NavLink}>
-					<Link href="/top-songs">CHARTS</Link>
-				</li>
-				<li className={NavStyles.NavLink}>
-					<Link href="/about">TOP SONGS</Link>
-				</li>
-			</ul>
-		</div>
-	)
-}
+  return (
+    <div className="flex w-5/6 m-auto my-6 p-6 bg">
+      <h1 className="text-5xl mr-24 text-primary font-black tracking-tighter my-auto">
+        MURE
+      </h1>
+      <ul className={NavStyles.NavLinkContainer}>
+        <div className="absolute top-2 left-2 bg-accent w-full h-full -z-10 rounded-full"></div>
+        <div className="absolute top-4 left-4 bg-secondary w-full h-full -z-20 rounded-full"></div>
+        <li className={NavStyles.NavLink}>
+          <Link href="/">HOME</Link>
+        </li>
+        <li className={NavStyles.NavLink}>
+          <Link href="/top-artists">CHARTS</Link>
+        </li>
+        <li className={NavStyles.NavLink}>
+          <Link href="/charts">ABOUT</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Nav;
--- config/route.ts
+++ config/route.ts
@@ -1,6 +1,19 @@
-export const route: {name: string, to: string}[] = [
-	{ 
-		name: "HOME",
-		to: '/home'
-	}
-]
+export const route: { name: string; to: string }[] = [
+  {
+    name: "HOME",
+    to: "/home",
+  },
+  {
+    name: "TOP ARTISTS",
+    to: "/top-artists",
+  },
+  {
+    name: "CHARTS",
+    to: "/charts",
+  },
+  {
+    name: "CHARTS",
+    to: "/charts",
+  },
+];
+

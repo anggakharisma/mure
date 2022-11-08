6adc6a28ab5880bd2f7504b7489d447d17aaef33
anggakharisma <anggaputrakharisma@gmail.com>
2022-11-08 20:33:34 +0700

    fix: dont show layout on error page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,12 @@ import Nav from "../components/Nav";
 import Head from "next/head";
 import { Player } from "../components/Player/Player";
 import HomeStyle from "../styles/Home.module.css";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  if (router.pathname === "/_error") return <Component {...pageProps} />;
+
   return (
     <>
       <Head>

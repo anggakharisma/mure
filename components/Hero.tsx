aefc4869100c44e6596afbca4feec1badd285adb
angga kharisma <anggaputrakharisma@gmail.com>
2023-02-03 14:24:24 +0700

    chore: custom seekbar(/) its working now

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,42 +3,46 @@ import HeroStyles from "../styles/Hero.module.css";
 import Triangle from "./Triangle";
 
 const Hero = () => {
-  return (
-    <div className={HeroStyles.hero}>
-      <Triangle
-        color="#9CCFE8"
-        strokeWidth={7}
-        width={104}
-        position="top-0 right-10"
-      />
-      <Triangle
-        color="#00FFB3"
-        strokeWidth={14}
-        width={50}
-        position="top-18 right-52"
-      />
-      <Triangle
-        color="#F5A9CB"
-        strokeWidth={7}
-        width={104}
-        position="right-1/2 bottom-0"
-      />
+	return (
+		<div className={HeroStyles.hero}>
+			<Triangle
+				color="#9CCFE8"
+				strokeWidth={7}
+				width={104}
+				position="top-0 right-10"
+			/>
 
-      <div className={HeroStyles.hero_image_container}>
-        <Image
-          src="/images/hero__image.jpg"
-          alt="hero image left"
-          layout="fill"
-          objectFit="contain"
-        />
-      </div>
-      <div className="z-30 m-auto">
-        <h1 className="z-30 w-2/4 font-bold leading-snug tracking-widest text-8xl ml-28 text-primary">
-          SHARE LISTEN DISCOVER
-        </h1>
-      </div>
-    </div>
-  );
+			<Triangle
+				color="#00FFB3"
+				strokeWidth={14}
+				width={50}
+				position="top-18 right-52"
+			/>
+
+			<Triangle
+				color="#F5A9CB"
+				strokeWidth={7}
+				width={104}
+				position="right-1/2 bottom-0"
+			/>
+
+			<div className={HeroStyles.hero_image_container}>
+				<Image
+					src="/images/hero__image.jpg"
+					alt="hero image left"
+					layout="fill"
+					objectFit="contain"
+				/>
+			</div>
+
+			<div className="z-30 m-auto">
+				<h1 className="z-30 w-2/4 font-bold leading-snug tracking-widest text-8xl ml-28 text-primary title-shadow">
+					SHARE LISTEN DISCOVER
+				</h1>
+			</div>
+
+		</div>
+	);
 };
 
 export default Hero;
--- components/Player/Control.tsx
+++ components/Player/Control.tsx
@@ -2,51 +2,66 @@ import { useRef, useState } from "react";
 import ControlStyle from "../../styles/Control.module.css";
 import Image from "next/image";
 
+
+// Reference
+// https://stackoverflow.com/questions/49814828/javascript-html5-audio-custom-players-seekbar-and-current-time
+
 enum PLAYER_STATE {
-  PLAY,
-  PAUSE,
-  STOP,
+	PLAY,
+	PAUSE,
+	STOP,
 }
 
 export const Control = () => {
-  const [playerState, setPlayerState] = useState<PLAYER_STATE>(
-    PLAYER_STATE.STOP
-  );
+	const [playerState, setPlayerState] = useState<PLAYER_STATE>(
+		PLAYER_STATE.STOP
+	);
+	const [songProgress, setSongProgress] = useState<number>(0);
 
-  const audioRef = useRef<HTMLAudioElement>(null);
+	const audioRef = useRef<HTMLAudioElement>(null);
 
-  return (
-    <div className={ControlStyle.ControlContainer}>
-      <div className="flex justify-center items-center">
-        <div className="relative hover:cursor-pointer">
-          <Image src="/images/prev.svg" width={40} height={40} />
-        </div>
-        <audio ref={audioRef} src="/audio_test.mp3" preload="metadata" loop />
-        <p
-          className="mx-8"
-          onClick={() => {
-            if (
-              playerState === PLAYER_STATE.STOP ||
-              playerState === PLAYER_STATE.PAUSE
-            ) {
-              audioRef.current?.play();
-              audioRef.current!.volume = 0.2;
-              setPlayerState(PLAYER_STATE.PLAY);
-            } else {
-              audioRef.current?.pause();
-              setPlayerState(PLAYER_STATE.PAUSE);
-            }
-          }}
-        >
-          {playerState == PLAYER_STATE.PAUSE || playerState == PLAYER_STATE.STOP
-            ? "pl"
-            : "pl"}
-        </p>
-        <div className="relative hover:cursor-pointer">
-          <Image src="/images/next.svg" width={40} height={40} />
-        </div>
-      </div>
-      <div className="w-3/4 m-auto bg-gray-200 h-1"></div>
-    </div>
-  );
+	return (
+		<div className={ControlStyle.ControlContainer}>
+			<div className="flex items-center justify-center">
+				<div className="relative hover:cursor-pointer">
+					<Image alt="previous song" src="/images/prev.svg" width={40} height={40} />
+				</div>
+				<audio
+					onChange={(e) => {
+
+					}}
+					onTimeUpdate={(e) => {
+						setSongProgress(audioRef.current!.currentTime / audioRef.current!.duration * 100);
+					}} onLoadedData={(e) => {
+					}} ref={audioRef} src="/audio_test.mp3" preload="metadata" loop />
+				<p
+					className="mx-8"
+					onClick={() => {
+						if (
+							playerState === PLAYER_STATE.STOP ||
+							playerState === PLAYER_STATE.PAUSE
+						) {
+							audioRef.current?.play();
+							audioRef.current!.volume = 0.2;
+							setPlayerState(PLAYER_STATE.PLAY);
+						} else {
+							audioRef.current?.pause();
+							setPlayerState(PLAYER_STATE.PAUSE);
+						}
+					}}
+				>
+					{playerState == PLAYER_STATE.PAUSE || playerState == PLAYER_STATE.STOP
+						? "pl"
+						: "ps"}
+				</p>
+				<div className="relative hover:cursor-pointer">
+					<Image alt="next song" src="/images/next.svg" width={40} height={40} />
+				</div>
+			</div>
+			<div className="w-3/4 h-1 m-auto">
+				<input className="w-full h-1 m-auto" type="range" value={songProgress} min="0" max="100" step="1" />
+			</div>
+
+		</div>
+	);
 };

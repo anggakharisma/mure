dbd349a22daf831b32a1c60b39801ffb8656b171
angga kharisma <anggaputrakharisma@gmail.com>
2023-02-04 14:46:11 +0700

    chore: add prettier config

--- components/Player/Control.tsx
+++ components/Player/Control.tsx
@@ -20,15 +20,17 @@ export const Control = () => {
 	const [currentTime, setCurrentTime] = useState<string>("00:00");
 	const [duration, setDuration] = useState<string>("00:00");
 	const [mouseDownSlider, setMouseDownSlider] = useState<boolean>(false);
+	const [volume, setVolume] = useState<number>(0.2);
 
 	const audioRef = useRef<HTMLAudioElement>(null);
 
 	useEffect(() => {
+		audioRef.current!.volume = Number(volume)
 		if (mouseDownSlider) {
 			const pct = Number(songProgress) / 100;
 			audioRef.current!.currentTime = (audioRef.current!.duration || 0) * pct;
 		}
-	}, [mouseDownSlider, songProgress])
+	}, [mouseDownSlider, songProgress, volume])
 
 
 	const fmtTime = (second: number) => {
@@ -48,10 +50,7 @@ export const Control = () => {
 				<div className="relative hover:cursor-pointer">
 					<Image alt="previous song" src="/images/prev.svg" width={40} height={40} />
 				</div>
-				<audio
-					onChange={(e) => {
-
-					}}
+				<audio autoPlay={false}
 					onTimeUpdate={(e) => {
 						if (!mouseDownSlider) {
 							setSongProgress(audioRef.current!.currentTime / audioRef.current!.duration * 100);
@@ -66,12 +65,12 @@ export const Control = () => {
 				<p
 					className="mx-8"
 					onClick={() => {
+						audioRef.current!.volume = volume;
 						if (
 							playerState === PLAYER_STATE.STOP ||
 							playerState === PLAYER_STATE.PAUSE
 						) {
 							audioRef.current?.play();
-							audioRef.current!.volume = 0.2;
 							setPlayerState(PLAYER_STATE.PLAY);
 						} else {
 							audioRef.current?.pause();
@@ -87,14 +86,17 @@ export const Control = () => {
 					<Image alt="next song" src="/images/next.svg" width={40} height={40} />
 				</div>
 			</div>
-			<div className="w-3/4 h-1 m-auto">
-				<span>{currentTime} / </span>
-				<span>{duration}</span>
-				<input className="w-full h-4 m-auto" type="range" value={songProgress} min="0" max="100" step="1" onChange={(e) => {
+			<div className="flex items-center justify-center w-3/4 m-auto">
+				<p className="w-1/5">{currentTime} / {duration}</p>
+				<input className="w-3/4 m-auto mr-4" type="range" value={songProgress} min="0" max="100" step="1" onChange={(e) => {
 					setSongProgress(e.target.value);
 				}}
 					onMouseDown={() => { setMouseDownSlider(true) }}
 					onMouseUp={() => { setMouseDownSlider(false) }}
+				/>
+				<input className="w-20 h-full m-auto mr-4" type="range" value={volume} min="0" max="1" step="0.1" onChange={(e) => {
+					setVolume(Number(e.target.value));
+				}}
 				/>
 			</div>
 
--- components/Player/Player.tsx
+++ components/Player/Player.tsx
@@ -3,12 +3,12 @@ import { Control } from "./Control";
 import { Info } from "./Info";
 
 export const Player = () => {
-  return (
-    <div className={PlayerStyles.PlayerContainer}>
-      <div className="m-auto w-3/4 flex">
-        <Info />
-        <Control />
-      </div>
-    </div>
-  );
+	return (
+		<div className={PlayerStyles.PlayerContainer}>
+			<div className="m-auto w-3/4 flex">
+				<Info />
+				<Control />
+			</div>
+		</div>
+	);
 };

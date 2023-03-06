4f4b80193673f3463e66d286e62b4f7fdc0e02ee
angga kharisma <anggaputrakharisma@gmail.com>
2023-03-06 12:49:49 +0700

    chore: charts section responsive

--- components/Charts/Charts.tsx
+++ components/Charts/Charts.tsx
@@ -2,39 +2,39 @@ import { featuredSongs, newSongs } from "../../data";
 import SongCard from "../SongCard/SongCard";
 
 const Charts = () => {
-	return (
-		<div className="w-full lg:mt-3.5 mt-[40rem]">
-			<div className="w-10/12 p-4 m-auto mt-16 mb-24" id="charts">
-				<div>
-					<h1 className="text-center lg:text-left mb-16 lg:mb-4 text-4xl font-semibold tracking-[40px] lg:tracking-widest text-primary lg:text-6xl max-w-full">
-						CHARTS
-					</h1>
-					<div className="flex flex-wrap items-center align-middle justify-items-stretch">
-						<div className="flex-1">
-							<h2 className="p-4 text-3xl font-medium tracking-widest text-center text-white bg-primary">
-								FEATURED SONGS
-							</h2>
-							<div className="justify-between w-full my-4 font-medium text-white">
-								{featuredSongs.map((info: ArtistInfo, index: number) => {
-									return <SongCard artistInfo={info} key={index} />;
-								})}
-							</div>
-						</div>
-						<div className="flex-1">
-							<h2 className="p-4 text-3xl font-medium tracking-widest text-center text-white bg-primary">
-								FEATURED SONGS
-							</h2>
-							<div className="justify-between w-full my-4 font-medium text-white">
-								{featuredSongs.map((info: ArtistInfo, index: number) => {
-									return <SongCard artistInfo={info} key={index} />;
-								})}
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div >
-	);
+  return (
+    <div className="w-full lg:mt-3.5 mt-[40rem]">
+      <div className="w-10/12 p-4 m-auto mt-16 mb-24" id="charts">
+        <div>
+          <h1 className="text-center lg:text-left mb-16 lg:mb-4 text-4xl font-semibold tracking-[40px] lg:tracking-widest text-primary lg:text-6xl max-w-full">
+            CHARTS
+          </h1>
+          <div className="flex flex-wrap items-center align-middle justify-items-stretch">
+            <div className="flex-1">
+              <h2 className="p-4 text-3xl font-medium tracking-widest text-center text-white bg-primary">
+                FEATURED SONGS
+              </h2>
+              <div className="justify-between w-full my-4 font-medium text-white">
+                {featuredSongs.map((info: ArtistInfo, index: number) => {
+                  return <SongCard artistInfo={info} key={index} />;
+                })}
+              </div>
+            </div>
+            <div className="flex-1">
+              <h2 className="p-4 text-3xl font-medium tracking-widest text-center text-white bg-primary">
+                FEATURED SONGS
+              </h2>
+              <div className="justify-between w-full my-4 font-medium text-white">
+                {featuredSongs.map((info: ArtistInfo, index: number) => {
+                  return <SongCard artistInfo={info} key={index} />;
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Charts;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,25 +9,25 @@ import Link from "next/link";
 import AudioProvider from "../context/audioPlayerContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	const router = useRouter();
-	if (router.pathname === "/_error") return <Component {...pageProps} />;
+  const router = useRouter();
+  if (router.pathname === "/_error") return <Component {...pageProps} />;
 
-	return (
-		<AudioProvider>
-			<Head>
-				<title>MURE</title>
-			</Head>
-			<Nav />
-			<div className={HomeStyle.GradientBg}></div>
-			<Component {...pageProps} />
-			<Link href="/">
-				<div className="fixed z-30 p-2 text-white transition-transform bg-gray-700 cursor-pointer hover:scale-110 right-12 bottom-40">
-					UP
-				</div>
-			</Link>
-			<Player />
-		</AudioProvider>
-	);
+  return (
+    <AudioProvider>
+      <Head>
+        <title>MURE</title>
+      </Head>
+      <Nav />
+      <div className={HomeStyle.GradientBg}></div>
+      <Component {...pageProps} />
+      <Link href="/">
+        <div className="fixed z-30 p-2 text-white transition-transform bg-gray-700 cursor-pointer hover:scale-110 right-12 bottom-40">
+          UP
+        </div>
+      </Link>
+      <Player />
+    </AudioProvider>
+  );
 }
 
 export default MyApp;

bd76aa2cb6b80bb83ce990bbb4456ba97e7b76e9
angga kharisma <anggaputrakharisma@gmail.com>
2023-03-06 14:44:45 +0700

    chore: random animation

--- components/Triangle.tsx
+++ components/Triangle.tsx
@@ -1,31 +1,35 @@
 const Triangle = ({
-  color,
-  width,
-  position,
-  strokeWidth
+	color,
+	width,
+	position,
+	strokeWidth,
+	duration
 }: {
-  color: string;
-  width: number;
-  position: string;
-  strokeWidth: number;
+	color: string;
+	width: number;
+	position: string;
+	strokeWidth: number;
+	duration: number;
 }) => {
-  return (
-    <div className={`spin absolute ${position}`}>
-      <svg
-        width={width}
-        height="104"
-        viewBox="0 0 90 104"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M4.02851 97.7448L4.02851 6.81218L82.7785 52.2785L4.02851 97.7448Z"
-          stroke={color}
-          strokeWidth={strokeWidth}
-        />
-      </svg>
-    </div>
-  );
+	return (
+		<div style={{
+			animationDuration: ` ${duration}s`
+		}} className={`z-50 spin absolute ${position}`}>
+			<svg
+				width={width}
+				height="104"
+				viewBox="0 0 90 104"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					d="M4.02851 97.7448L4.02851 6.81218L82.7785 52.2785L4.02851 97.7448Z"
+					stroke={color}
+					strokeWidth={strokeWidth}
+				/>
+			</svg>
+		</div>
+	);
 };
 
 export default Triangle;

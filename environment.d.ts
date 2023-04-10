ce61802cd6b88f64dd015657b85ba66fce437d33
angga kharisma <anggaputrakharisma@gmail.com>
2023-04-10 20:38:44 +0700

    fix: pretty

--- environment.d.ts
+++ environment.d.ts
@@ -1,8 +1,8 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-			GOOGLE_CLIENT_ID: string;
-			GOOGLE_CLIENT_SECRET: string;
+      GOOGLE_CLIENT_ID: string;
+      GOOGLE_CLIENT_SECRET: string;
     }
   }
 }
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,12 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
   providers: [
-		GoogleProvider({
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-			clientId: process.env.GOOGLE_CLIENT_ID as string
-		})
-  ],
-}
-export default NextAuth(authOptions)
+    GoogleProvider({
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.GOOGLE_CLIENT_ID as string
+    })
+  ]
+};
+export default NextAuth(authOptions);

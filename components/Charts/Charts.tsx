026ca199d5e7b95610b8937e9cbc9a62ebaacd52
angga <angga@dev>
2023-04-17 12:02:16 +0700

    chore: add play icon on song card

--- components/Charts/Charts.tsx
+++ components/Charts/Charts.tsx
@@ -6,12 +6,12 @@ const Charts = () => {
     <div className="w-full mx-auto md:w-5/6 lg:mt-3.5 mt-[24rem] md:mt-[40rem]">
       <div className="w-full mb-24 md:p-4 md:mt-16 md:m-auto" id="charts">
         <div>
-          <h1 className="text-center lg:text-left mb-16 lg:mb-4 text-4xl font-semibold tracking-[32px] lg:tracking-widest text-primary lg:text-6xl max-w-full">
+          <h1 className="lg:text-left mb-16 lg:mb-4 font-semibold tracking-[32px] lg:tracking-widest text-primary lg:text-5xl text-4xl mx-auto text-center">
             CHARTS
           </h1>
           <div className="flex flex-wrap items-center align-middle justify-items-stretch mb-40">
             <div className="flex-1">
-              <h2 className="p-4 text-3xl font-medium tracking-widest text-center text-white bg-primary">
+              <h2 className="p-4 text-2xl font-medium tracking-widest text-center text-white bg-primary rounded-tl-full rounded-bl-full">
                 FEATURED SONGS
               </h2>
               <div className="justify-between w-full my-4 font-medium text-white">
@@ -21,7 +21,7 @@ const Charts = () => {
               </div>
             </div>
             <div className="flex-1">
-              <h2 className="p-4 text-3xl font-medium tracking-widest text-center text-white bg-primary">
+              <h2 className="p-4 text-2xl font-medium tracking-widest text-center text-white bg-primary rounded-tr-full rounded-br-full">
                 NEW SONGS
               </h2>
               <div className="justify-between w-full my-4 font-medium text-white">
--- components/SongCard/SongCard.tsx
+++ components/SongCard/SongCard.tsx
@@ -18,6 +18,22 @@ const SongCard = ({ artistInfo }: SongCardProps) => {
       >
         <div className="top-0 bottom-0 left-0 z-10 w-full h-full transition-all bg-black opacity-0 none group-hover:opacity-40 group-hover:absolute hover:cursor-pointer"></div>
         <Image layout="fill" src={image} objectFit="cover" alt="whatever" />
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 text-4xl bg-dark-primary border-white border-solid  border-2 rounded-full p-4 opacity-85 hover:cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2.5}
+            stroke="#ffffff"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+            />
+          </svg>
+        </div>
       </div>
       <div>
         <p className="font-medium text-primary">{artistName}</p>

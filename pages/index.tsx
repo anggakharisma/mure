78af1ce9118b28b43d49bde05ff6dfb002e211ad
angga <angga@dev>
2023-04-21 21:23:55 +0700

    chore: remove this will use oAuth from backend a.k.a spring boot

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,8 @@
 import type { NextPage } from "next";
-import { useSession, signIn, signOut } from "next-auth/react";
-import { useState } from "react";
-import NewWindow from "react-new-window";
 import Charts from "../components/Charts/Charts";
 import Hero from "../components/Hero";
 
 const Home: NextPage = () => {
-  const { data: session, status } = useSession();
-  const [popup, setPopUp] = useState<boolean>(false);
   return (
     <>
       <Hero />

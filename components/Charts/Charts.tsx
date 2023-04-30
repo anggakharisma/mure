1ab9e84f857b789d543423a082588884216c658e
angga kharisma <anggaputrakharisma@gmail.com>
2023-04-30 09:00:43 +0700

    chore: add basic nav on mobile

--- components/Charts/Charts.tsx
+++ components/Charts/Charts.tsx
@@ -3,15 +3,15 @@ import SongCard from "../SongCard/SongCard";
 
 const Charts = () => {
   return (
-    <div className="w-full mx-auto md:w-5/6 lg:mt-3.5 mt-[30rem] md:mt-[24rem]">
+    <div className="w-full mx-auto md:w-5/6 lg:mt-3.5 mt-[35rem] md:mt-[32rem]">
       <div className="w-full mb-24 md:p-4 md:mt-16 md:m-auto" id="charts">
         <div>
-          <h1 className="lg:text-left mb-16 lg:mb-4 font-semibold tracking-[32px] lg:tracking-widest text-primary lg:text-5xl md:text-4xl text-3xl mx-auto text-center">
+          <h1 className="mx-auto my-8 tracking-[16px] text-3xl font-semibold text-center lg:text-left lg:mb-4 lg:tracking-[32px] text-primary lg:text-5xl md:text-4xl">
             CHARTS
           </h1>
           <div className="flex flex-wrap items-center align-middle justify-items-stretch">
             <div className="flex-1">
-              <h2 className="p-4 text-2xl font-medium tracking-widest text-center text-white bg-primary rounded-tl-full rounded-bl-full">
+              <h2 className="p-4 text-2xl font-medium tracking-widest text-center text-white bg-primary">
                 FEATURED SONGS
               </h2>
               <div className="justify-between w-full my-4 font-medium text-white">
@@ -21,7 +21,7 @@ const Charts = () => {
               </div>
             </div>
             <div className="flex-1">
-              <h2 className="p-4 text-2xl font-medium tracking-widest text-center text-white bg-primary rounded-tr-full rounded-br-full">
+              <h2 className="p-4 text-2xl font-medium tracking-widest text-center text-white bg-primary">
                 NEW SONGS
               </h2>
               <div className="justify-between w-full my-4 font-medium text-white">
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,7 +4,7 @@ import NavStyles from "../styles/Nav.module.css";
 
 const Nav = () => {
   return (
-    <div className="flex w-5/6 p-6 m-auto my-6">
+    <div className="flex items-center justify-between w-5/6 p-6 m-auto my-6 align-middle md:justify-start">
       <h1 className="my-auto mr-24 text-5xl font-black tracking-tighter text-primary">
         MURE
       </h1>
@@ -23,6 +23,12 @@ const Nav = () => {
           <p className="hover:cursor-pointer">SIGN IN</p>
         </li>
       </ul>
+
+			<div className="hidden h-full cursor-pointer sm:block md:hidden">
+				<div className="w-2 h-2 rounded-full bg-primary"></div>
+				<div className="w-2 h-2 my-2 rounded-full bg-secondary"></div>
+				<div className="w-2 h-2 rounded-full bg-accent"></div>
+			</div>
     </div>
   );
 };

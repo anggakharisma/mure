3cffd27b74b087fb7a90a6ef255ae6bed8a7b3c3
anggakharisma <anggaputrakharisma@gmail.com>
2024-03-19 12:16:38 +0700

    chore: Footer section

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,27 @@
+
+const Footer = () => {
+  return (
+    <div className="w-5/6 mx-auto flex gap-12 items-center align-middle">
+      <h1 className="my-auto text-3xl font-[900] tracking-tighter text-primary">
+        MURE
+      </h1>
+      <div className="relative w-5/6 mx-auto h-full p-12 rounded-lg  my-12 bg-primary before:w-full before:h-full before:-top-4 before:-right-4 before:bg-accent before:absolute before:-z-10 before:rounded-lg after:w-full after:h-full after:-top-8 after:-right-8 after:bg-secondary after:absolute after:-z-20 after:rounded-lg">
+        <div className="flex gap-8">
+          <div>
+            <h2 className="text-white font-bold text-lg mb-2">SITE</h2>
+            <p className="text-white">Jobs</p>
+            <p className="text-white">Artist</p>
+          </div>
+          <div>
+            <h2 className="text-white font-bold text-lg mb-2">HELP</h2>
+            <p className="text-white">Developers</p>
+            <p className="text-white">Privacy Policy</p>
+            <p className="text-white">Cookie Policy</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer;

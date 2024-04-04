184829829e31d4598d23f447db640daeda494433
anggakharisma <anggaputrakharisma@gmail.com>
2024-04-05 04:37:40 +0700

    whoops

---
+++ components/HlsPlayer/HlsPlayer.tsx
@@ -0,0 +1,9 @@
+const HlsPlayer = () => {
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure neque commodi qui eos ad! Ut blanditiis veritatis sequi inventore delectus dolor doloremque molestias similique et, est culpa amet ducimus in?</p>
+    </div>
+  )
+}
+
+export default HlsPlayer;

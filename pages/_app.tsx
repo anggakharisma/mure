e5f96744a9fa28571c5fcb5d9fd8216b0d9cbea7
anggakharisma <anggakharisma@protonmail.com>
2026-03-24 13:55:34 +0700

    chore:

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import { Session } from "next-auth";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import Link from "next/link";
 import { useRouter } from "next/router";
 import AuthModal from "../components/Auth/AuthModal";
 import Nav from "../components/Nav";
@@ -21,6 +20,7 @@ function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+
           />
           <title>MURE</title>
         </Head>
